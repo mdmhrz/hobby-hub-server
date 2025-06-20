43dc0704cd67e46ad8b563ab74ae92831725b578
mdmhrz <razufreelance@gmail.com>
2025-06-21 05:57:45 +0600

    Server side running and initial setup done

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('HobbyHub is on sky');
+})
+
+app.listen(port, () => {
+    console.log(`hobbyHub Serveris running in the port${port}`)
+});
