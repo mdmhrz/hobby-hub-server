835cb87a787c49d4f8649c705e544ed2897b16a2
mdmhrz <razufreelance@gmail.com>
2025-06-22 08:19:36 +0600

    Vercel deployment done

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
+
+
 app.use(cors());
 app.use(express.json());
 
@@ -25,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const groupCollection = client.db('groupsDB').collection('groups')
 
@@ -86,8 +88,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
